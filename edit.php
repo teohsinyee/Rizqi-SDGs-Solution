8a6c497a99b5130fab9cb59928e575ef9ec86653
sinyee2000 <56106630+sinyee2000@users.noreply.github.com>
2022-01-15 15:20:40 +0800

    profile info & listings done

---
+++ edit.php
@@ -0,0 +1,31 @@
+<?php
+
+include('connection.php');  
+
+$id = $_GET['id']; 
+
+$qry = mysqli_query($conn,"SELECT * FROM `POST` where 'POST_ID'='$id'"); // select query
+
+$data = mysqli_fetch_array($qry); 
+
+if(isset($_POST['update'])) // when click on Update button
+{
+    $quantity = $_POST['quantity'];
+
+    $query = "UPDATE `post` SET `POST_QUANTITY` = '$quantity' WHERE `post`.`POST_ID` = '$id'";  
+  if ($conn->query($query) === TRUE) {
+   echo "Record updated successfully";
+   header("location:profileinfo.php");
+ } else {
+   echo "Error updating record: " . $conn->error;
+ }
+
+}
+?>
+
+<h3>Update Data</h3>
+
+<form method="POST">
+  <input type="number" name="quantity" value="<?php echo $data['POST_QUANTITY'] ?>" placeholder="Enter Quantity" Required>
+  <input type="submit" name="update" value="Update">
+</form>
--- profileinfo.php
+++ profileinfo.php
@@ -1,7 +1,12 @@
 <?php 
 include('connection.php'); 
 session_start();
+
+$_SESSION['userid'] = 1;
+$id = $_SESSION['userid'];
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -14,44 +19,58 @@ session_start();
 <body>
 <h1>My Profile details</h1>
 
-<!--display name,email,phone number
-1. select display specific user
-2. USER_ID should be dynamic based on the login session ID - derived USER_ID from email
-3. ASSIGN SESSION[USER_ID]=USER_ID.....
-4. The query should be..... USER_ID = $SESSION['USER_ID'].....
--->
 <div>
     <h2>Here is the user profile details</h2>
 <?php  
-   $query = "SELECT * FROM `user` WHERE `USER_ID`='2'";  
-   $results = mysqli_query($conn, $query); //return object
-	if (mysqli_num_rows($results) == 1) { //if there is something in the result
-        $data=mysqli_fetch_assoc($results);} //array
+   $query = "SELECT * FROM `user` WHERE `USER_ID`='$id'";  
+   $results = mysqli_query($conn, $query);
+	if (mysqli_num_rows($results) == 1) { 
+        $data=mysqli_fetch_assoc($results);} 
 echo $data['USER_NAME'] . " ".$data['USER_EMAIL']. " ".$data['USER_PHONE_NUMBER'];
 ?>
 </div>
 
-<!--Show post id that link to this user
-1. select from post DB & where user_ID = '''''
--->
-<div>
-    <h2>Here is the listing</h2>
-<?php  
-   $query = "SELECT * FROM `POST` WHERE `USER_ID`='4'";  #need to make a loop here
-   $results = mysqli_query($conn, $query); //return object
-	if (mysqli_num_rows($results) == 1) { //if there is something in the result
-        $data=mysqli_fetch_assoc($results);} //array
-echo $data['POST_ITEM_NAME'] . " ".$data['POST_DESCRIPTION']. " ".$data['POST_QUANTITY']. " ".$data['POST_PICTURE'];
-?>
 <!--A form enable to user to update the quantity
 0. DELETE FROM `post` WHERE `post`.`POST_ID` = 18"
 1. UPDATE `post` SET `POST_QUANTITY` = '3' WHERE `post`.`POST_ID` = 2
 2. POST_QUANTITY shud be dynamic & take user input
 Extra: Field validation - becuz if user keyin string then it will cause error when insert
 input into SQL statement
-3. https://stackoverflow.com/questions/13882656/html-form-in-php-while-loop
 -->
+<!--Make a listings table here -->
+
+<div>
+    <h2>My Listings</h2>
+    <table border="2">
+  <tr>
+    <td>Item ID</td>
+    <td>Item name</td>
+    <td>Item description</td>
+    <td>Item quantity</td>
+    <td>Edit</td>
+    <td>Delete</td>
+  </tr>
+
+  <?php  
+  $query = "SELECT * FROM `POST` WHERE `USER_ID`='$id'";  
+  $results = mysqli_query($conn, $query); 
+  while($data = mysqli_fetch_array($results))
+{
+?>
+<tr>
+    <td><?php echo $data['POST_ID']; ?></td>
+    <td><?php echo $data['POST_ITEM_NAME']; ?></td>
+    <td><?php echo $data['POST_DESCRIPTION']; ?></td>
+    <td><?php echo $data['POST_QUANTITY']; ?></td>    
+    <td><a href="edit.php?id=<?php echo $data['POST_ID']; ?>">Edit</a></td>
+    <td><a href="delete.php?id=<?php echo $data['POST_ID']; ?>">Delete</a></td>
+  </tr>
+  <?php
+}
+?>
+</table>
 
 </div>
+
 </body>
 </html>
