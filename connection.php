a33339244e5f87e01c807cc714a4db45e0a7b03c
sinyee2000 <56106630+sinyee2000@users.noreply.github.com>
2022-01-27 16:00:33 +0800

    No changes

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "Rizqi";
+$conn = mysqli_connect($servername, $username, $password,$db);
+
+// Check connection
+/*if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+}
+echo nl2br("\nConnected successfully to " . $db . " database.\n");*/
+
+?>
