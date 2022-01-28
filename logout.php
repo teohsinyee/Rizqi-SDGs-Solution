7f7c992e8f52b9131c6a48fe3ec530ec26d82742
Siti Sakinah <69177804+ssakinah@users.noreply.github.com>
2022-01-29 05:54:52 +0800

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    session_destroy(); // destroy the session
-    unset($_SESSION['username']); //remove all session variables
-    header('location: login_form.php'); //header() function sends a raw HTTP header to a client.
-
-  ?>
