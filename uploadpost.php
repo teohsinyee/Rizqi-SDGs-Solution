875bec88bebd7f5461fe336b905fd48b65b18117
sinyee2000 <56106630+sinyee2000@users.noreply.github.com>
2022-01-30 14:53:50 +0800

    edit insert query and add alert for success post

--- uploadpost.php
+++ uploadpost.php
@@ -1,7 +1,9 @@
 <?php
 include('connection.php');  
-
 session_start();
+
+$id = $_SESSION['userID'];
+
 $status = $statusMsg = ''; 
 
 if (isset($_POST['insert'])) {
@@ -13,25 +15,25 @@ if (isset($_POST['insert'])) {
     
      // Allow certain file formats 
      $allowTypes = array('jpg','png','jpeg', 'JPG', 'PNG', 'JPEG'); 
-if(in_array($fileType, $allowTypes)){ 
-        $image = $_FILES['itemimage']['tmp_name']; 
-        $imgContent = (addslashes(file_get_contents($image)));     
+    if(in_array($fileType, $allowTypes)){ 
+            $image = $_FILES['itemimage']['tmp_name']; 
+            $imgContent = (addslashes(file_get_contents($image)));     
 
-        $itemname = $_REQUEST['itemname']; 
-        $itemdescription= $_REQUEST['itemdescription'];
-        $itemquantity = $_REQUEST['itemquantity'];
-        $itemlocation = $_REQUEST['itemlocation'];
-        $itemcategory = $_REQUEST['itemcategory'];
-        $date = date("Y\-m\-d");
-        $time = date("H:i:s");
+            $itemname = $_REQUEST['itemname']; 
+            $itemdescription= $_REQUEST['itemdescription'];
+            $itemquantity = $_REQUEST['itemquantity'];
+            $itemlocation = $_REQUEST['itemlocation'];
+            $itemcategory = $_REQUEST['itemcategory'];
+
 
         $query = 
         "INSERT INTO `post` (`POST_ID`, `USER_ID`, `POST_ITEM_NAME`, `POST_DESCRIPTION`, `POST_PICTURE`, `POST_QUANTITY`, 
-        `POST_LOCATION`, `POST_CATEGORY`, `POST_DATE`, `POST_TIME`) 
-        VALUES (NULL, '1', '$itemname', '$itemdescription','$imgContent' , '$itemquantity', 
-        '$itemlocation', '$itemcategory', '$date', '$time')";
+        `POST_LOCATION`, `POST_CATEGORY`, `POST_DATETIME`) 
+        VALUES (NULL, '$id', '$itemname', '$itemdescription','$imgContent' , '$itemquantity', 
+        '$itemlocation', '$itemcategory', current_timestamp())";
 
-        if($conn->query($query) === TRUE){ 
+
+        if($conn->query($query) === TRUE){ //check INSERT success or not
             $status = 'success'; 
             $statusMsg = "File uploaded successfully."; 
         }else{ 
@@ -40,13 +42,14 @@ if(in_array($fileType, $allowTypes)){
 }else{ 
     $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.'; 
 } 
-}else{ 
-$statusMsg = 'Please select an image file to upload.'; 
-} 
+    }else{ $statusMsg = 'Please select an image file to upload.'; } 
 
 }
-// Display status message 
-echo $statusMsg; 
-echo "<script>location.href='createpost.php'</script>";
+
+
+echo ("<script LANGUAGE='JavaScript'>
+    window.alert('Post successfully created!');
+    window.location.href='index.html';
+    </script>");
 
 ?>
