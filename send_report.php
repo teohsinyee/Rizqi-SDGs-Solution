4b70a131aa876e808ae70336bc81b97333edb520
Aliff Iskandar <Muhammad_Aliff_Iskandar@protonmail.com>
2022-02-01 13:09:43 +0800

    Changed modal popup to not display by default and added session user ID to insert query in report table

--- send_report.php
+++ send_report.php
@@ -3,7 +3,7 @@ $db_handle = mysqli_connect("localhost", "root", "", "rizqi");
 
 
 $report_id = null;
-$reporting_user_id = 1;
+$reporting_user_id = $_SESSION['userID'];
 $post_id = $_POST['reporting_post_id'];
 
 $post_owner_user_id_result = mysqli_query($db_handle, "SELECT `USER_ID` FROM `post` WHERE `POST_ID` = $post_id;");
