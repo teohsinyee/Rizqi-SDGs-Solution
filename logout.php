aebbf3a4b8ac989b88f4454be41a63da30517de1
sinyee2000 <56106630+sinyee2000@users.noreply.github.com>
2022-02-07 12:26:29 +0800

    Remove all files
    All files migrated to branch 'Heroku rosak'

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    session_destroy(); // destroy the session
-    unset($_SESSION['username']); //remove all session variables
-    unset($_SESSION['userID']); 
-    unset($_SESSION['logged_in']);
-    header('location: login_form.php'); //header() function sends a raw HTTP header to a client.
-
-  ?>
